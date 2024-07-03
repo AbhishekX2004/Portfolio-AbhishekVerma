802537beafbadd0a6b2e1636955625729ab1181a
Abhishek Verma <abhishekaverma20@gmail.com>
2024-07-03 17:14:44 +0530

    Hashlink add

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const resumeURL = "https://drive.google.com/file/d/1eUPE63Txs3QzKhJ2zZFdpdWNr1X3X8g6/view?usp=sharing";
 
@@ -8,7 +9,7 @@ function Header(props) {
     <nav className="header-nav">
       <Link to="/"><img src="AIcon3.png" height="90px" alt="A logo" className="header-logo" /></Link>
       <div className="header-right">
-        <a href='/#aboutMe' className="flat-button" style={{marginRight:0}}>About Me</a>
+        <HashLink smooth to='/#aboutMe' className="flat-button" style={{marginRight:0}}>About Me</HashLink>
         <a href={resumeURL} target="_blank" rel="noopener noreferrer" className="flat-button">Resume</a>
         <Link to="/contactMe" className="contact-link">Contact Me</Link>
       </div>
